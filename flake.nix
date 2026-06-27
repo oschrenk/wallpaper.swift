0217a5415e31c52c80a9834b74c8024f555c8e47
Oliver Schrenk <oliver.schrenk@gmail.com>
2026-06-27 13:32:18 +0200

    chore: setup nix

---
+++ flake.nix
@@ -0,0 +1,18 @@
+{
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+
+  outputs =
+    { nixpkgs, ... }:
+    {
+      devShells.aarch64-darwin.default =
+        let
+          pkgs = nixpkgs.legacyPackages.aarch64-darwin;
+        in
+        pkgs.mkShell {
+          packages = with pkgs; [
+            go-task # task runner 
+            swiftlint # swift, lint
+          ];
+        };
+    };
+}
